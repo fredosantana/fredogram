e50da73a7a951ea135e3662f7090330b39ea06b0
Wil Santana <santana.wil@gmail.com>
2016-04-24 12:32:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fredogram::Application.config.secret_key_base = 'f086787e7b430608af96ba5908f80502cdd427fe6c5556e3bc5066d7f9b29d6b2d91ef32696bf6ef99ea92d3deb9a4749329951242c8cf0804aad2b034f79224'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fredogram::Application.config.session_store :cookie_store, key: '_fredogram_session'

1914671a74f0401cce350af761e301fae8c0c5ed
Wil Santana <santana.wil@gmail.com>
2016-04-26 01:24:14 +0000

    Comments controller added

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe CommentsController, type: :controller do
+	describe "comments#create action" do
+   		it "should allow users to create comments on grams" do
+      
+    	end
+    
+    	it "should require a user to be logged in to comment on a gram" do
+      
+    	end
+
+    	it "should return http status code of not found if the gram isn't found" do
+
+    	end
+  	end
+end
